ccbcc7d7cbc9c5f7ea183ffaa0ea23e96139962f
Jonah Engelund <jonahengelund@gmail.com>
2018-11-09 14:52:00 -0700

    project init

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -4,12 +4,28 @@ import ItunesService from "./itunes-service.js";
 
 const itunesService = new ItunesService()
 
-function drawSongs(results) {
-  console.log(results)
+function drawSongs(songs) {
+  console.log(songs)
   //YOUR CODING STARTS HERE
-
-
-
+  let template = ''
+  for (let i = 0; i < songs.length; i++) {
+    const song = songs[i]
+    template += `
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="${song.albumArt}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">${song.title}</p>
+        <p class="card-text">${song.artist}</p>
+        <p class="card-text">${song.collection}</p>
+        <p class="card-text">${song.price}</p>
+        <audio controls>
+        <source src="${song.preview}" type="audio/mpeg">
+        </audio>
+      </div>
+    </div>`
+  }
+  document.getElementById('song-list').innerHTML = template
 }
 
 
